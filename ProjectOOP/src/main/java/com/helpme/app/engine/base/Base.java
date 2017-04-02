7fd1e498f4bca833b9ad0abbeb13b468b03f7c00
ElloP <ollepersson629@gmail.com>
2017-04-02 17:40:40 +0200

    Added window logic and settings. Outputs a simple window to test.

--- ProjectOOP/src/main/java/com/helpme/app/engine/base/Base.java
+++ ProjectOOP/src/main/java/com/helpme/app/engine/base/Base.java
@@ -1,5 +1,9 @@
 package com.helpme.app.engine.base;
 
+import org.lwjgl.opengl.GL;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
 
 /**
  * Created by Olle on 2017-04-02.
@@ -10,6 +14,24 @@ public class Base {
     }
 
     private Base() {
+        simpleWindow();
+    }
 
+    private void simpleWindow() {
+        Window window = new Window();
+        window.initWindow(800,600, "Hello World!");
+
+        glClearColor(0.6f, 0.3f, 0.2f, 1.0f);
+
+        while(!window.shouldClose()) {
+
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            glfwPollEvents();
+
+            glfwSwapBuffers(window.getWindow());
+        }
+
+        window.destroy();
     }
 }
