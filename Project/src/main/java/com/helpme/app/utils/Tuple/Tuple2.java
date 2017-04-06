eee5e31dfbc1ff9899b7c43c230bc472de527dab
klas <klas@ludvigsson.se>
2017-04-06 13:03:48 +0200

    Tuple classes are done. Located in utils

--- Project/src/main/java/com/helpme/app/utils/Tuple/Tuple2.java
+++ Project/src/main/java/com/helpme/app/utils/Tuple/Tuple2.java
@@ -3,5 +3,11 @@ package com.helpme.app.utils.Tuple;
 /**
  * Created by og on 2017-04-06.
  */
-public class Tuple2 {
+public class Tuple2<A,B> {
+    public A a;
+    public B b;
+    public Tuple2(A a, B b){
+        this.a = a;
+        this.b = b;
+    }
 }
--- Project/src/main/java/com/helpme/app/utils/Tuple/Tuple3.java
+++ Project/src/main/java/com/helpme/app/utils/Tuple/Tuple3.java
@@ -3,5 +3,13 @@ package com.helpme.app.utils.Tuple;
 /**
  * Created by og on 2017-04-06.
  */
-public class Tuple3 {
+public class Tuple3 <A,B,C> {
+    public A a;
+    public B b;
+    public C c;
+    public Tuple3(A a, B b, C c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
 }
