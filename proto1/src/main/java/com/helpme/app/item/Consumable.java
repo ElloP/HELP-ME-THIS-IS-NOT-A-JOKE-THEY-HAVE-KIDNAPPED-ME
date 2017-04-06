ffe95e91fabeb8e25bc0d4f9b2d24fd35a5e37e2
kopa <enlitengas@gmail.com>
2017-04-06 12:18:14 +0200

    removed proto1 folder

--- proto1/src/main/java/com/helpme/app/item/Consumable.java
+++
@@ -1,7 +0,0 @@
-package com.helpme.app.item;
-
-/**
- * Created by Jacob on 2017-03-30.
- */
-public class Consumable {
-}
--- proto1/src/main/java/com/helpme/app/tile/edge/Edge.java
+++
@@ -1,7 +0,0 @@
-package com.helpme.app.tile.edge;
-
-/**
- * Created by Jacob on 2017-03-30.
- */
-public class Edge {
-}
