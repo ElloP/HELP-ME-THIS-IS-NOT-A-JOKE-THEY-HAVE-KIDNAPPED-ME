21ecfd677e629199aac97d415d22e419e77327b9
Jesper Persson <jesper.92.persson@gmail.com>
2017-04-06 13:39:48 +0200

    Now we ccan begin

--- Project/src/main/java/com/helpme/app/utils/mathl/Vector2f.java
+++ Project/src/main/java/com/helpme/app/utils/mathl/Vector2f.java
@@ -4,4 +4,11 @@ package com.helpme.app.utils.mathl;
  * Created by Jesper on 2017-04-06.
  */
 public class Vector2f {
+    private float x;
+    private float y;
+
+    public Vector2f(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
 }
