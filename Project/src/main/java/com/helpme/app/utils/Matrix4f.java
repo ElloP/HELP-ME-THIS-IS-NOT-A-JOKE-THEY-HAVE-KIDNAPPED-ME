370039e2cb462cdde5f4228345eced28dc3b312e
ElloP <ollepersson629@gmail.com>
2017-04-11 16:39:06 +0200

    Added matrix4f, vector3f wrappers for JOML

---
+++ Project/src/main/java/com/helpme/app/utils/Matrix4f.java
@@ -0,0 +1,139 @@
+package com.helpme.app.utils;
+
+import org.joml.*;
+
+import java.nio.FloatBuffer;
+
+/**
+ * Authored by Olle on 2017-04-11.
+ */
+
+// ----------- Wrapper for matrix4f -----------
+public class Matrix4f {
+    //
+    public org.joml.Matrix4f matrix; //TODO(Olle): set this to private
+
+    // ----------- Constructors -----------
+    public Matrix4f() {
+        matrix = new org.joml.Matrix4f();
+    }
+
+    public Matrix4f(FloatBuffer fb) {
+        matrix = new org.joml.Matrix4f(fb);
+    }
+
+    public Matrix4f(float m00, float m01, float m02, float m03,
+                    float m10, float m11, float m12, float m13,
+                    float m20, float m21, float m22, float m23,
+                    float m30, float m31, float m32, float m33) {
+
+        matrix = new org.joml.Matrix4f(m00, m01, m02, m03,
+                                       m10, m11, m12, m13,
+                                       m20, m21, m22, m23,
+                                       m30, m31, m32, m33);
+    }
+
+    public Matrix4f(Matrix4f mat) { //uses the private constructor below
+        this(mat.matrix);
+    }
+
+    private Matrix4f(org.joml.Matrix4f mat) { //private constructor for copying (and setting) matrices
+        this.matrix = mat;
+    }
+
+    public Matrix4f(Vector4f col0, Vector4f col1, Vector4f col2, Vector4f col3) {
+        //TODO(Olle): finish this constructor
+    }
+
+    // ----------- Operations/functions -----------
+
+    public void add(Matrix4f other) {
+        this.matrix.add(other.matrix);
+    }
+
+    public float determinant() {
+        if(this.matrix.isAffine()) {
+            return this.matrix.determinantAffine();
+        } else {
+            return this.matrix.determinant();
+        }
+    }
+
+    public boolean equals(Matrix4f other) {
+        return this.matrix.equals(other.matrix);
+    }
+
+    public void identity() {
+        this.matrix.identity();
+    }
+
+    public void invert() {
+        if(this.matrix.isAffine()) {
+            this.matrix.invertAffine();
+        } else {
+            this.invert();
+        }
+    }
+
+    public void lerp(Matrix4f other, float t) {
+        this.matrix.lerp(other.matrix, t);
+    }
+
+    public void multiply(Matrix4f other) {
+        if(this.matrix.isAffine()) {
+            this.matrix.mulAffine(other.matrix);
+        } else {
+            this.matrix.mul(other.matrix);
+        }
+    }
+
+    public void multiply(Vector3f v) {
+        //TODO(Olle): add matrix-vector multiplication
+    }
+
+    public void perspective(float fovy, float aspect, float zNear, float zFar) {
+        this.matrix.perspective(fovy, aspect, zNear, zFar);
+    }
+
+    public float perspectiveFar() {
+        return this.matrix.perspectiveFar();
+    }
+
+    public float perspectiveFov() {
+        return this.matrix.perspectiveFov();
+    }
+
+    public void rotate(float angle, float x, float y, float z) {
+        if(this.matrix.isAffine()) {
+            this.matrix.rotateAffine(angle, x, y, z);
+        } else {
+            this.matrix.rotate(angle, x, y, z);
+        }
+    }
+
+    public void rotate(float angle, Vector3f v) {
+        //TODO(Olle): add rotation by vector
+    }
+
+    public void scale(float xyz) {
+        this.matrix.scale(xyz);
+    }
+
+    public void scale(float x, float y, float z) {
+        this.matrix.scale(x, y, z);
+    }
+
+    public void scale(Vector3f v) {
+        //TODO(Olle): add scaling by vector
+    }
+
+    public void translate(float x, float y, float z) {
+        this.matrix.translate(x, y , z);
+    }
+
+    public void translate (Vector3f v) {
+        //TODO(Olle): add translation by vector
+    }
+
+
+}
---
+++ Project/src/main/java/com/helpme/app/utils/Vector3f.java
@@ -0,0 +1,7 @@
+package com.helpme.app.utils;
+
+/**
+ * Created by Olle on 2017-04-11.
+ */
+public class Vector3f {
+}
