6c873f3c873080b6fd507459d861881f4bf5e20c
klas <klas@ludvigsson.se>
2017-04-13 15:10:14 +0200

    Changed the dialogue to make more sense

--- Project/src/test/java/com/helpme/app/Mock/MockDialogue.java
+++ Project/src/test/java/com/helpme/app/Mock/MockDialogue.java
@@ -19,7 +19,7 @@ public class MockDialogue {
         mockDialogue[0] = new DialogueNode("What's up?", "Nothing much", null);
         mockDialogue[1] = new DialogueNode("Who are you?", "I am death, the destroyer of worlds", null);
         mockDialogue[2] = new DialogueNode("What can you tell me about this place?", "Nothing", null);
-        mockDialogue[3] = new DialogueNode("What is 5 times 5", "Smoke", null);
+        mockDialogue[3] = new DialogueNode("What is 5 times 5", "at least 10", null);
         mockDialogue[4] = new DialogueNode("Who is Olle?", "A jolly good fellow", null);
         dialogue = new Dialogue("Hello!",mockDialogue);
     }
