eb36f03139e46f36735d8aca9ddb4866c5ddf4cf
Jesper <jesper.92.persson@gmail.com>
2017-05-19 23:21:32 +0200

    Implemented abstract method in UIShader

--- Project/src/main/java/com/helpme/app/engine/renderer/base/UIShader.java
+++ Project/src/main/java/com/helpme/app/engine/renderer/base/UIShader.java
@@ -3,6 +3,7 @@ package com.helpme.app.engine.renderer.base;
 import com.helpme.app.engine.base.Camera;
 import com.helpme.app.engine.base.Transform;
 import com.helpme.app.engine.base.Window;
+import com.helpme.app.utils.mathl.Matrix4f;
 
 /**
  * Created by Jesper on 2017-05-12.
@@ -20,6 +21,12 @@ public class UIShader extends Shader {
     public void updateUniforms(Transform transform, Camera camera) {
         setUniform("model", transform.getModelMatrix());
         //setUniform("view", camera.getViewMatrix());
+        setUniform("projection", transform.getOrthoMatrix(0, Window.width, Window.height, 0, -1, 1.0f));
+    }
+
+    @Override
+    public void updateUniforms(Matrix4f model, Transform transform, Camera camera) {
+        setUniform("model", model);
         setUniform("projection", transform.getOrthoMatrix(0, Window.width, Window.height, 0, -1, 1.0f));
     }
 }
